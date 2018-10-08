ce1b2dac2baf097922b85e1b19c19bc59fa2006d
Malte Wildt <malte.wildt@flavia-it.de>
2018-10-08 18:10:09 +0200

    cleanup models

--- src/de/hsw/jee/guestbook01/model/GuestbookEntry.java
+++ src/de/hsw/jee/guestbook01/model/GuestbookEntry.java
@@ -11,37 +11,39 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-public class GuestbookEntry{
-	
+public class GuestbookEntry {
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	@ManyToOne
 	private User user;
-	
+
 	private String title;
-	
+
 	private String message;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date dateCreated;
-	
-	public GuestbookEntry(){}
-	
+
+	public GuestbookEntry() {
+	}
+
 	public GuestbookEntry(User user, String title, String message) {
 		this.user = user;
 		this.title = title;
 		this.message = message;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public User getUser() {
 		return user;
 	}
@@ -53,20 +55,25 @@ public class GuestbookEntry{
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
+
 	public Date getDateCreated() {
 		return dateCreated;
 	}
+
 	public void setDateCreated(Date dateCreated) {
 		this.dateCreated = dateCreated;
 	}
-	
+
 }
